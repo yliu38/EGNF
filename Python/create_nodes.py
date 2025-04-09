151050e6feb566607d93ca986b8e4b2b850b3184
yliu38 <90358625+yliu38@users.noreply.github.com>
2025-04-09 13:29:13 -0500

    Update create_nodes.py

--- Python/create_nodes.py
+++ Python/create_nodes.py
@@ -26,7 +26,7 @@ query2 = """
 
         WITH round(toFloat(csv.dendrogram_height),3,"HALF_UP") as height, csv.event_name as event_name, csv.gene_name as gene, toInteger(csv.event_no) as event, csv.include as incl, round(toFloat(csv.median_exp),3,"HALF_UP") as me, toInteger(csv.leaf_status) as leaf, split(csv.samples, ",") as spls, toInteger(csv.no_of_samples) as no_spl, toInteger(csv.levels) as levels
 
-        CREATE (:Expression {name: gene, event_name: event_name, event_no: event, dendrogram_height: height, median_exp: me, leaf_status: leaf, samples: spls, no_of_unique_samples: no_spl, include: incl, levels: levels});
+        CREATE (:Expression:class1 {name: gene, event_name: event_name, event_no: event, dendrogram_height: height, median_exp: me, leaf_status: leaf, samples: spls, no_of_unique_samples: no_spl, include: incl, levels: levels});
 
         ',
 
